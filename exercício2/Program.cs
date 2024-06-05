c05261f6208dd78dc06fa5c280afad4e6a0a8cef
marianyrocha <152741522+marianyrocha@users.noreply.github.com>
2024-06-05 17:46:49 -0400

    Add files via upload

---
+++ exercício2/Program.cs
@@ -0,0 +1,23 @@
+﻿// Exercício 2
+// Média aritmetica entre 3 valores:
+
+int numeroUm = 0;
+int numeroDois = 0;
+int numeroTres = 0;
+int soma = 0;
+double mediaAritmetica = 0;
+
+Console.WriteLine("Digite o primero número: ");
+numeroUm = int.Parse(Console.ReadLine());
+
+Console.WriteLine("Digite o segundo número: ");
+numeroDois = int.Parse(Console.ReadLine());
+
+Console.WriteLine("Digite o terceiro número: ");
+numeroTres = int.Parse(Console.ReadLine());
+
+soma = (numeroUm + numeroDois + numeroTres);
+Console.WriteLine("A soma dos três valores é: " + soma);
+
+mediaAritmetica = soma / 3;
+Console.WriteLine("Logo, o resultado da média aritmetica será: " + mediaAritmetica); 
