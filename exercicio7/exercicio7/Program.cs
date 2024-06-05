8748513b95be1e8882da25fc1b335d4eae83e33f
marianyrocha <152741522+marianyrocha@users.noreply.github.com>
2024-06-05 18:19:20 -0400

    Add files via upload

---
+++ exercicio7/exercicio7/Program.cs
@@ -0,0 +1,24 @@
+﻿// Exercício 7
+// Verificar se um número é zero, par ou ímpar!
+
+int numero = 0;
+
+Console.WriteLine("Digite um número: ");
+numero = int.Parse(Console.ReadLine());
+
+if (numero == 0)
+{
+    Console.WriteLine("O número " + numero + " é nulo!");
+}
+else
+{
+    if (numero % 2 == 0)
+    {
+        Console.WriteLine("O número " + numero + " é par!");
+    }
+    else
+    {
+        Console.WriteLine("O número " + numero + " é ímpar!");
+    }
+}
+
