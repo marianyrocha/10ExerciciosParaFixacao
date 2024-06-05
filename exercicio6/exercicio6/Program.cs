c501ba70c64f95177c0998a66ed31519f20f8881
marianyrocha <152741522+marianyrocha@users.noreply.github.com>
2024-06-05 18:14:38 -0400

    Add files via upload

---
+++ exercicio6/exercicio6/Program.cs
@@ -0,0 +1,16 @@
+﻿// Exercício 6
+// Verificar se um número é par ou ímpar!
+
+int numero = 0;
+
+Console.WriteLine("Digite um número: ");
+numero = int.Parse(Console.ReadLine());
+
+if (numero % 2 == 0)
+{
+    Console.WriteLine("O número " + numero + " é par!");
+}
+else
+{
+    Console.WriteLine("O número " + numero + " é ímpar!");
+}
